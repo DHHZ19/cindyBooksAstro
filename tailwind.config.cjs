265e29710e41c60c92cc8b70cd4e16b9d1a377ad
Diego Hernandez <diegofootball19@gmail.com>
2023-01-15 17:51:27 -0600

    color changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        cindypink: {
+          950: "247	206	205",
+        },
+      },
       fontFamily: {
         body: ["Porsche Next"],
       },
