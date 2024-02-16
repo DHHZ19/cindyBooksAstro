beeddc8470cdd0aa8c8a986cd8fa525028388370
Diego Hernandez <diegofootball19@gmail.com>
2024-02-16 17:02:37 -0600

    delet curator

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,3 @@
-/* curator-feed-default-feed-layout */
-(function () {
-  var i,
-    e,
-    d = document,
-    s = "script";
-  i = d.createElement("script");
-  i.async = 1;
-  i.charset = "UTF-8";
-  i.src =
-    "https://cdn.curator.io/published/a7e920c3-a7c7-473a-8a86-24d28089d8d8.js";
-  e = d.getElementsByTagName(s)[0];
-  e.parentNode.insertBefore(i, e);
-})();
-
 // Burger menus
 document.addEventListener("DOMContentLoaded", function () {
   // open
